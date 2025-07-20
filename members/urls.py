81605a4aaae697d2d8f9c0402cb20875e97575e5
Justinahadji <justinkuffour96@gmail.com>
2025-07-20 09:21:35 +0300

    files

--- members/urls.py
+++ members/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views  #
 
 urlpatterns = [
+     path('', views.main, name='main'),
      path('', views.members, name='members'),
      path('details/<int:id>', views.details, name='details'),
 ]
